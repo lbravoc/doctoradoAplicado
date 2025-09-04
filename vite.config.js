030d5abe06ded1303c8aa1792bb438ea9a10715c
Loreto Bravo <lbravoc@users.noreply.github.com>
2025-09-04 15:19:48 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/proyectosEmpresas/', // 👈 importante
+  base: '/doctoradoAplicado/', // 👈 importante
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
